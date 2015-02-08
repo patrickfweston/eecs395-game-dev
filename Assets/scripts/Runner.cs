0ef6a9beaa3b5f418b329f36eb622be6dce2e2fd
xytan123 <xytan123@gmail.com>
2015-02-07 18:33:09 -0600

    *

--- Assets/scripts/Runner.cs
+++ Assets/scripts/Runner.cs
@@ -11,6 +11,14 @@ public class Runner : MonoBehaviour {
 			move_left();
 			
 		}
+		if (Input.GetKey("up")) {
+			move_forward();
+			
+		}
+		if (Input.GetKey("down")) {
+			move_back();
+			
+		}
 	}
 
 	void move_right () {
@@ -19,5 +27,11 @@ public class Runner : MonoBehaviour {
 
 	void move_left () {
 		transform.Translate(-5f * Time.deltaTime, 0f, 0f);
+	}
+	void move_forward () {
+		transform.Translate(0f, 0f,5f * Time.deltaTime);
+	}
+	void move_back () {
+		transform.Translate( 0f, 0f,-5f * Time.deltaTime);
 	}
 }
