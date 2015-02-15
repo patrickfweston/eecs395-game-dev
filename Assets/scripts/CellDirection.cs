0c74681d698ac2621d3c5d722c1599912904967c
xytan123 <xytan123@gmail.com>
2015-02-15 01:23:36 -0600

    uml included

---
+++ Assets/scripts/CellDirection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CellDirection {
+	North,
+	East,
+	South,
+	West
+}
+
+public static class CellDirections {
+	
+	public const int Count = 4;
+	
+	private static IntVector2[] vectors = {
+		new IntVector2(0, 1),
+		new IntVector2(1, 0),
+		new IntVector2(0, -1),
+		new IntVector2(-1, 0)
+	};
+	
+	public static IntVector2 ToIntVector2 (this CellDirection direction) {
+		return vectors[(int)direction];
+	}
+
+	private static CellDirection[] opposites = {
+		CellDirection.South,
+		CellDirection.West,
+		CellDirection.North,
+		CellDirection.East
+	};
+	
+	public static CellDirection GetOpposite (this CellDirection direction) {
+		return opposites[(int)direction];
+	}
+}
--- Assets/scripts/Map.cs
+++ Assets/scripts/Map.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class Map : MonoBehaviour {
 
 	public IntVector2 size;
 	
 	public MapCell cellPrefab;
+	public Passage passagePrefab;
+	public Wall wallPrefab;
 
 	private MapCell[,] cells;
 
 	public float generationStepDelay;
+
+	public MapCell GetCell (IntVector2 coordinates) {
+		return cells[coordinates.x, coordinates.z];
+	}
+
+	private bool CoordMakeSense(IntVector2 coordinates){
+		return (coordinates.x >=0 && coordinates.z >=0 
+		        && coordinates.x <size.x && coordinates.z < size.z);
 	
+	}
+
 	public IEnumerator Generate () {
 		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
 		cells = new MapCell[size.x, size.z];
@@ -19,11 +32,26 @@ public class Map : MonoBehaviour {
 			for (int z = 0; z < size.z; z++) {
 				yield return delay;
 				CreateCell(new IntVector2(x, z));
+
+				MapCell currentCell = cells[x,z];
+
+				foreach(CellDirection direction in Enum.GetValues(typeof(CellDirection))){
+					IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2();
+					MapCell neighbor = null;
+					if(CoordMakeSense(coordinates)){
+						neighbor = GetCell(coordinates);
+					}
+
+					if(neighbor != null) {
+						CreatePassage(currentCell, neighbor, direction);
+					}
+				}
+
 			}
 		}
 	}
 	
-	private void CreateCell (IntVector2 coordinates) {
+	private MapCell CreateCell (IntVector2 coordinates) {
 		MapCell newCell = Instantiate(cellPrefab) as MapCell;
 		cells[coordinates.x, coordinates.z] = newCell;
 		newCell.coordinates = coordinates;
@@ -31,6 +59,23 @@ public class Map : MonoBehaviour {
 		newCell.transform.parent = transform;
 		newCell.transform.localPosition =
 			new Vector3(coordinates.x - size.x * 0.5f + 0.5f, -0.5f, coordinates.z - size.z * 0.5f + 0.5f);
+		return newCell;
+	}
+
+	private void CreatePassage (MapCell cell, MapCell otherCell, CellDirection direction) {
+		Passage passage = Instantiate(passagePrefab) as Passage;
+		passage.Initialize(cell, otherCell, direction);
+		passage = Instantiate(passagePrefab) as Passage;
+		passage.Initialize(otherCell, cell, direction.GetOpposite());
+	}
+	
+	private void CreateWall (MapCell cell, MapCell otherCell, CellDirection direction) {
+		Wall wall = Instantiate(wallPrefab) as Wall;
+		wall.Initialize(cell, otherCell, direction);
+		if (otherCell != null) {
+			wall = Instantiate(wallPrefab) as Wall;
+			wall.Initialize(otherCell, cell, direction.GetOpposite());
+		}
 	}
 
 }
