cd762bcc83ecccb52645dd5b0d28df2063840b24
patrickfweston <patrickfweston@gmail.com>
2015-02-16 17:53:51 -0600

    Started the bribing interface

---
+++ Assets/scripts/BribeInterface.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BribeInterface : MonoBehaviour {
+
+	public GUIText bribeText, bribeHelp, bribeAmount, bribeSubtitle, bribeInstructions;
+
+	// Use this for initialization
+	void Start () {
+		bribeText.enabled = false;
+		bribeHelp.enabled = false;
+		bribeAmount.enabled = false;
+		bribeSubtitle.enabled = false;
+		bribeInstructions.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
