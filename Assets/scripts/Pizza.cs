d5a96f481b68cb288f8683c23f12ac0a2f1350c8
xytan123 <xytan123@gmail.com>
2015-03-09 16:50:42 -0500

    sound

--- Assets/scripts/Pizza.cs
+++ Assets/scripts/Pizza.cs
@@ -3,11 +3,39 @@ using System.Collections;
 
 public class Pizza : MonoBehaviour {
 
-	void OnCollisionEnter(Collision col){
+	public AudioClip Eating_sound;
+	
+	
+	public AudioSource s;
+	
+	
+	void Awake () {
+		
+//		source = GetComponent<AudioSource>();
+
+	}
+	
+
+
+	IEnumerator OnCollisionEnter(Collision col){
 //		Debug.Log ("got pizza");
+
 		if (col.gameObject.name == "Runner") {
-			Destroy(gameObject);
+
 			Runner.incrementPizzaBy(1);
+
+//			source.pitch = Random.Range (lowPitchRange,highPitchRange);
+//			float hitVol = col.relativeVelocity.magnitude * velToVol;
+
+			s.clip = Eating_sound;
+			s.Play();
+
+			Debug.Log("played sound");
+			renderer.enabled = false;
+			gameObject.GetComponent<BoxCollider>().enabled = false;
+			yield return new WaitForSeconds(s.clip.length);
+//			SelfDe
+			Destroy(gameObject);
 		}
 	}
 
